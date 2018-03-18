99a828ef78d2f152e85aa824f8286740aead6de6
AlanDG96 <alan.mlg@hotmail.com>
2018-03-17 22:28:48 -0600

    'subiendo'

---
+++ Datos/Validador.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Datos
+{
+    public class Validador
+    {
+        public Boolean esEntero(string entero)
+        {
+            int x = Int32.Parse(entero);
+            if (x % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public Boolean esDecimal(string deci)
+        {
+            if (deci.Contains("."))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean esEdad(string edad)
+        {
+            int x = Int32.Parse(edad);
+            if (x < 110)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean esDigito(String digito)
+        {
+            Regex r = new Regex(@"^[A-Za-z]+$");
+            String d = digito;
+            if (r.IsMatch(d))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Vista/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Validador va = new Validador();
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+            string c = textBox3.Text;
+            string d = textBox4.Text;
+
+            int val = 0;
+            int inval = 0;
+
+            if (va.esEntero(a) == true)
+            {
+                val++;
+            }
+            else
+            {
+                inval++;
+            }
+            if (va.esDecimal(b) == true)
+            {
+                val++;
+            }
+            else
+            {
+                inval++;
+            }
+
+            if (va.esEdad(c) == true)
+            {
+                val++;
+            }
+            else
+            {
+                inval++;
+            }
+            if (va.esDigito(d) == true)
+            {
+                val++;
+            }
+            else
+            {
+                inval++;
+            }
+            MessageBox.Show("Válidos:\t" + val + "\nIválidos:\t" + inval);
+        }
+    }
+}
